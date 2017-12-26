e799f66c69d59a5afce8a9501280aa8273a8142b
Brandon David <brandonhudavid@berkeley.edu>
2017-12-26 03:20:10 -0800

    add left slide & nav bar

--- bd2.js
+++ bd2.js
@@ -7,8 +7,38 @@ window.addEventListener("load", function()
 })
 
 function shift(){
-    document.getElementById("overlay").classList.add("slideOut")
+    document.getElementById("overlay").classList.add("slideDown")
     document.getElementById("col1").classList.add("col1slide")
     document.getElementById("col2").classList.add("col2slide")
     document.getElementById("col3").classList.add("col3slide");
+}
+
+function select1(){
+    document.getElementById("col1").onclick = function(){return false;}
+    document.getElementById("col2").onclick = function(){return false;}
+    document.getElementById("col3").onclick = function(){return false;}
+    document.getElementById("col1").classList.add("slideIn")
+    document.getElementById("col2").classList.add("slideLeft2")
+    document.getElementById("col3").classList.add("slideLeft")
+    document.getElementById("nav").classList.add("slideNav");
+}
+
+function select2(){
+    document.getElementById("col1").onclick = function(){return false;}
+    document.getElementById("col2").onclick = function(){return false;}
+    document.getElementById("col3").onclick = function(){return false;}
+    document.getElementById("col1").classList.add("slideLeft")
+    document.getElementById("col2").classList.add("slideIn2")
+    document.getElementById("col3").classList.add("slideLeft")
+    document.getElementById("nav").classList.add("slideNav");
+}
+
+function select3(){
+    document.getElementById("col1").onclick = function(){return false;}
+    document.getElementById("col2").onclick = function(){return false;}
+    document.getElementById("col3").onclick = function(){return false;}
+    document.getElementById("col1").classList.add("slideLeft")
+    document.getElementById("col2").classList.add("slideLeft2")
+    document.getElementById("col3").classList.add("slideIn")
+    document.getElementById("nav").classList.add("slideNav");
 }
