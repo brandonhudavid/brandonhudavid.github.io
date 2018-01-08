d28c03e9d8e4c641cb242d9a478285cc3668e06d
Brandon David <brandonhudavid@berkeley.edu>
2018-01-07 23:20:50 -0800

    file optimize

--- old/bd.js
+++
@@ -1,9 +0,0 @@
-//var overlay = document.getElementById("overlay");
-//
-//window.addEventListener('load', function(){
-//    overlay.style.display = "none"
-//})
-
-document.getElementsByClassName('.wrapper').each(function(index, element) {
-        element.classList.remove('loading');
-    });
