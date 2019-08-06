1c91fa43bf5495f8feb4f5beec412b413791985d
Brandon <brandon.david@optimizely.com>
2019-08-05 17:39:06 -0700

    hashrouter

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3dfc6e1279269a73c5a70e39ebeb0db8.js"
+  "/precache-manifest.22c7d562c2202007b486815051f9b311.js"
 );
 
 self.addEventListener('message', (event) => {
